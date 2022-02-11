09f0efbfec7245bcb74e2dd4de621c0b992f4d97
Ivan Nikolsky <enty8080@gmail.com>
2022-02-12 00:24:03 +0300

    Update json.c

--- src/json.c
+++ src/json.c
@@ -106,7 +106,60 @@ static JSONObject * _parseJSON(string str, int * offset)
                 _offset += i + 2;
             }
             obj->pairs[obj->count - 1] = tempPtr;
+        } else if(*str == '\'') {
             
+            int i = strNextOccurence(++str, '"');
+            if (i <= 0) {
+                freeJSONFromMemory(obj);
+                return NULL;
+            }
+            
+            JSONPair tempPtr = obj->pairs[obj->count - 1];
+            
+            tempPtr.key = newWithSize(character , i + 1);
+            memcpy(tempPtr.key, str, i * sizeof(character));
+            tempPtr.key[i] = '\0';
+            
+            str += i + 1;
+            _offset += i + 2;
+            
+            i = strNextOccurence(str, ':');
+            if (i == -1)
+                return NULL;
+            str += i + 1;
+            _offset += i + 1;
+            
+            removeWhitespaceCalcOffset(str, _offset);
+            
+            if (*str == '{') {
+                int _offsetBeforeParsingChildObject = _offset;
+                int _sizeOfChildObject;
+
+                tempPtr.value = new(JSONValue);
+                tempPtr.type = JSON_OBJECT;
+                tempPtr.value->jsonObject = _parseJSON(str, &_offset);
+                if (tempPtr.value->jsonObject == NULL) {
+                    freeJSONFromMemory(obj);
+                    return NULL;
+                }
+
+                _sizeOfChildObject = _offset - _offsetBeforeParsingChildObject;
+                str += _sizeOfChildObject;
+            } else if (*str == '\'') {
+                i = strNextOccurence(++str, '\'');
+                if (i == -1) {
+                    freeJSONFromMemory(obj);
+                    return NULL;
+                }
+                tempPtr.value = new(JSONValue);
+                tempPtr.type = JSON_STRING;
+                tempPtr.value->stringValue = newWithSize(character, i + 1);
+                memcpy(tempPtr.value->stringValue, str, i * sizeof(character));
+                tempPtr.value->stringValue[i] = '\0';
+                str += i + 1;
+                _offset += i + 2;
+            }
+            obj->pairs[obj->count - 1] = tempPtr;
         } else if (*str == ',') {
             obj->count++;
             obj->pairs = renewWithSize(obj->pairs, JSONPair, obj->count);
